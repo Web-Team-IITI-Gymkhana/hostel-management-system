1cce9acb3feec9ea93f2dcf1cb30232975b6291b
Trishant Kumar Talluri <trishanttalluri@gmail.com>
2024-03-03 15:03:25 +0530

    added frontend repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-2xl text-center p-5">Hostel Management Website</div>
+      <div className="border-black border-2 text-center text-xl">Navbar</div>
+    </>
+  )
+}
+
+export default App
