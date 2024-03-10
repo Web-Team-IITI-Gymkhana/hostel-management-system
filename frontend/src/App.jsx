9c8841afb6084b0c3a9c7b69b7128e3ad72c858b
chiranjivi <chiranjivikeshav1@gmail.com>
2024-03-10 23:36:29 +0530

    Routing set-up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Auth from './components/Auth';
+import Rules from './components/Rules';
+import Booking from './components/Booking';
+import Complaints from './components/Complaints';
+import Contact from './components/Contact';
 
 function App() {
-
   return (
-    <>
-     <Navbar/>
-      {/* <div className="text-2xl text-center p-5">Hostel Management Website</div>
-      <div className="border-black border-2 text-center text-xl">Navbar</div> */}
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Auth" element={<Auth />} />
+          <Route path="/Rules" element={<Rules />} />
+          <Route path="/Booking" element={<Booking />} />
+          <Route path="/Complaints" element={<Complaints />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
