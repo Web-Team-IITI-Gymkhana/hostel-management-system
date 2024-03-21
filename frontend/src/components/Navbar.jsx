19573f46ee94e8108f37a9db630d4d40e1cc5881
chiranjivi <chiranjivikeshav1@gmail.com>
2024-03-21 20:31:25 +0530

    Profile page updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AuthContext from '../context/AuthContext.jsx';
 import '../css/Navbar.css';
 import logo from '../images/Indian_Institute_of_Technology,_Indore_Logo.png';
-import { useState,useContext } from 'react';
-
+import { useState, useContext } from 'react';
+import 'flowbite';
 function Navbar() {
     const navigate = useNavigate();
-    let {user,logout} = useContext(AuthContext)
+    let { user, logout } = useContext(AuthContext)
     const [isMenuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => {
         setMenuOpen(!isMenuOpen);
         console.log("clicked")
-    }; 
+    };
     const handleClick = () => {
         // Close the menu when clicking on the link
         setMenuOpen(false);
-        
+
         // Navigate to the '/profile' route
         navigate('/profile');
-      };
+    };
+    const [isDropdownOpen, setDropdownOpen] = useState(true);
+    const toggleDropdown =()=>{
+            setDropdownOpen(!isDropdownOpen)
+    }
     return (
         <>
             <div className="logo bg-white py-2 px-0 mx-md-3 mx-0 container-md">
@@ -34,41 +38,55 @@ function Navbar() {
                 </a>
             </div>
             <div className="navigation">
-                <nav className="bg-[rgb(18, 37, 106)] w-full z-20">
-                    <div className="flex flex-wrap items-center justify-between px-5 mx-auto">
-                    <div className="flex py-2 md:order-2 space-x-3 md:space-x-0 w-screen justify-between md:w-max">
-                        <button onClick={toggleMenu} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-800" aria-controls="navbar-sticky" aria-expanded="false">
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-                            </svg>
-                        </button>
-                        {user===null ? <Link to='/auth' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center text-xl">Login</Link>
-                        : <button onClick={logout} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center text-xl">Logout</button>}
-                    </div>
-                    <div className={`items-center justify-between ${isMenuOpen? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
-                        <ul className="flex flex-col p-2 md:p-0 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-                        <li>
-                            <Link onClick={() => setMenuOpen(false)} to='' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl" aria-current="page">Home</Link>
-                        </li>
-                        <li>
-                            <Link onClick={() => setMenuOpen(false)} to='/rules' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Rules</Link>
-                        </li>
-                        <li>
-                            <Link onClick={() => setMenuOpen(false)} to='/booking' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Booking</Link>
-                        </li>
-                        <li>
-                            <Link onClick={() => setMenuOpen(false)} to='/complaints' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Complaints</Link>
-                        </li>
-                        <li>
-                            <Link onClick={() => setMenuOpen(false)} to='/contact' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Contact</Link>
-                        </li>
-                        { user&&
-                        <li>
-                            <Link  to='/profile' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Profile</Link>
-                        </li>}
-                        </ul>
-                    </div>
+                <nav className="bg-[rgb(18, 37, 106)] w-full z-20 h-14">
+                    <div className="flex flex-wrap justify-between px-5 mx-auto items-start">
+                        <div className="flex py-2 md:order-2 space-x-3 md:space-x-0 w-screen justify-between md:w-max">
+                            <button onClick={toggleMenu} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-800" aria-controls="navbar-sticky" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                                </svg>
+                            </button>
+                            {!user ? <Link to='/auth' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center text-xl">Login</Link>
+                                :
+                            <div className='flex flex-col items-end'>
+                            <div onClick={toggleDropdown} class=" relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 cursor-pointer">
+                                <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20"  xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                            </div>
+                            
+                            <div class={`z-10 ${!isDropdownOpen?'hidden':''} bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 mt-1`}>
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                                    <li>
+                                        <Link to='/profile' class="block px-4 py-2 hover:text-blue-500">Profile</Link>
+                                    </li>
+                                    <li >
+                                         <button onClick={logout} class="block px-4 py-2 hover:text-blue-500">Logout</button>
+                                    </li>
+                                </ul>
+                            </div>
+                            </div>
+                            } 
+                        </div>
+                        <div className={`items-center justify-between ${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
+                            <ul className="flex flex-col p-2 md:p-0 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
+                                <li>
+                                    <Link onClick={() => setMenuOpen(false)} to='' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl" aria-current="page">Home</Link>
+                                </li>
+                                <li>
+                                    <Link onClick={() => setMenuOpen(false)} to='/rules' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Rules</Link>
+                                </li>
+                                <li>
+                                    <Link onClick={() => setMenuOpen(false)} to='/booking' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Booking</Link>
+                                </li>
+                                <li>
+                                    <Link onClick={() => setMenuOpen(false)} to='/complaints' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Complaints</Link>
+                                </li>
+                                <li>
+                                    <Link onClick={() => setMenuOpen(false)} to='/contact' className="block py-2 md:py-0 px-3 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-xl">Contact</Link>
+                                </li>
+                               
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </div>
--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -5,9 +5,27 @@ function Profile() {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className='text-2xl'>
-      Hello {user.email} !!
-    </div>
+    <>
+      <div className='flex flex-wrap'>
+        <div className='w-[360px] h-[400px] mx-3 my-3 rounded-lg p-6 border '>
+          <p className='font-bold h-10'>Your Profile</p>
+          <div class="relative inline-flex items-center justify-center w-20 h-20 overflow-hidden bg-orange-500 rounded-full">
+            <span class="font-extrabold text-gray-600 dark:text-gray-300">CK</span>
+          </div>
+          <p className='font-medium'>Chiranjivi Kehsav</p>
+          <p className=''>Roll Number : 220001022</p>
+          <p className=''>Email : cse220001022@iiti.ac.in</p>
+          <p className=''>Department : CSE</p>
+          <hr className='my-3  border-gray-400 '/>
+          <p className=' py-1'>Hostel Name : VSB</p>
+          <p className=' py-1'>Unit Number : 309</p>
+          <p className=' py-1'>Room Type : D</p>
+        </div>
+        <div className='w-[360px] mx-3 my-3 h-64 rounded-lg p-6 border '>
+
+        </div>
+      </div>
+    </>
   );
 }
 
